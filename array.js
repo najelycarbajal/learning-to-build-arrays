5c380a967fb08402ffe6e7dc3d864b0765837414
najely carbajal <najelyc55996@students.laalliance.org>
2017-11-17 17:22:34 +0000

    learning to organize list using arrays

---
+++ array.js
@@ -0,0 +1,11 @@
+var countries = [
+  "Algeria",
+  "Brazil",
+  "Canada",
+  "Denmark",
+  "USA"
+]
+countries.push("Agentina")
+countries.push("Greece")
+countries.push("Portugal")
+console.log(countries)
